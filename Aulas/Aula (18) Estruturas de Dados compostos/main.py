3bac1c3449349b833e8ced7c02cf00dfe27e0af3
felipebernardi711-ship-it <felipebernardi711@gmail.com>
2026-08-28 09:16:15 -0300

    Add files via upload

---
+++ Aulas/Aula (18) Estruturas de Dados compostos/main.py
@@ -0,0 +1,7 @@
+'''Estruturas de dados compostas'''
+
+turma = [["Enzo", "Pereira bobão", "Ana"],
+         ["Fernando", "Pedrão", "Samuca"],
+         ["Padeiro", "Acorda", "Bluezao"]]
+
+print(turma[2][2])
