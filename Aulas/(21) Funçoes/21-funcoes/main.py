324d7cbe52922e1d3f21b12e2383160be5a0194c
felipebernardi711-ship-it <felipebernardi711@gmail.com>
2026-09-10 12:08:21 -0300

    Add files via upload

---
+++ Aulas/(21) Funçoes/21-funcoes/main.py
@@ -0,0 +1,11 @@
+print()
+int()
+# Funções são blocos de código que podem ser reutilizados
+def calcular_media():
+    print("Calculando média...")
+   
+def saudacao(nome):
+    print(f"Bem vindo! {nome} ")
+   
+calcular_media()
+saudacao("Jonatan")
