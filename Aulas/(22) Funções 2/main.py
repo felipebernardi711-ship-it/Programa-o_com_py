cc0ab957dddf99e2f33c4b714d51a1b5f9474945
felipebernardi711-ship-it <felipebernardi711@gmail.com>
2026-09-17 10:29:33 -0300

    Add files via upload

---
+++ Aulas/(22) Funções 2/main.py
@@ -0,0 +1,10 @@
+
+
+def apresentar(nome, idade):
+    print("Olá", nome, "Sua idade é", idade)
+def somar(n1, n2):
+    return n1 + n2
+    
+apresentar("Adenilson", 33)
+apresentar("Joao A", 38)
+apresentar("Motoka", 19)
